93a72e69cd2bccf533ea6515a02b8c58a1ca412d
Karshilov <karshilov@foxmail.com>
2022-04-09 23:44:06 +0800

    fix: declare module

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,3 +1,9 @@
-import { Config } from "./types";
-declare function SpiderProgress(begin?: string, target?: string, config?: Config): void;
-export default SpiderProgress;
+declare module 'progress-spider' {
+    type Config = {
+        left: number;
+        bottom: number;
+        style: any;
+    };
+    function SpiderProgress(begin?: string, target?: string, config?: Config): void;
+    export default SpiderProgress;
+}
